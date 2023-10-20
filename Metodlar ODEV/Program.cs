199e8f7135940d1e15dcc450437fd59b7d8fdca0
ismai <ismai@LAPTOP-J44951SB>
2023-10-20 17:11:50 +0300

    Add project files.

---
+++ Metodlar ODEV/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodlar_ODEV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarpimTablosu(10);
+
+            Console.ReadLine();
+        }
+    
+    static void CarpimTablosu(int boyut)
+            
+        {
+        
+            for(int i=1;i<=boyut;i++)
+            {
+                for(int j = 1; j <= boyut; j++)
+                {
+                    Console.WriteLine(i + "*" + j + "="+ i * j);
+                    
+                }
+                Console.WriteLine("\n");
+            }
+          
+        
+        
+        
+        
+        }
+    
+    
+    
+    }
+}
